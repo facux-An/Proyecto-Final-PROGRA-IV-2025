87816585cd19dc73e032ab4bc12c367de4984e49
facux-an <fandrada153@gmail.com>
2025-11-24 10:53:34 -0300

    a ver si aparecen estas imagenes de mierda la puta que lo pario

--- productos/admin.py
+++ productos/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Producto
+from django.utils.html import format_html
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
@@ -23,3 +24,9 @@ class ProductoAdmin(admin.ModelAdmin):
     def hay_stock(self, obj):
         return "✅ Sí" if obj.stock > 0 else "❌ No"
     hay_stock.short_description = "Disponible"
+    
+    def imagen_portada(self, obj):
+        if obj.portada:
+            return format_html('<img src="{}" width="80" style="border-radius:4px;" />', obj.portada.url)
+        return "Sin imagen"
+    imagen_portada.short_description = "Portada"
