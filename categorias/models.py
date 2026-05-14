dbd06fb056de250cce024c97bea835da01dbf765
facux-an <fandrada153@gmail.com>
2026-05-14 19:20:26 -0300

    Actualizacion de frontend 1.2

--- categorias/models.py
+++ categorias/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
     descripcion = models.TextField(blank=True, null=True)  # campo opcional para detalles
+    imagen = models.ImageField(upload_to='categorias/', blank=True, null=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
     class Meta:
