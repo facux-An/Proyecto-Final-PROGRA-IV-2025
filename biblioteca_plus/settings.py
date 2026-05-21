8625e66a906573494eee9c90c1d50cef46b88548
facux-an <fandrada153@gmail.com>
2026-05-21 14:14:55 -0300

    config: agregar nuevos dominios tiendaplus.com.ar a CSRF_TRUSTED_ORIGINS

--- biblioteca_plus/settings.py
+++ biblioteca_plus/settings.py
@@ -28,6 +28,8 @@ else:
 
 CSRF_TRUSTED_ORIGINS = [
     "https://proyecto-final-progra-iv-2025.onrender.com",
+    "https://tiendaplus.com.ar",
+    "https://www.tiendaplus.com.ar",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
 ]
