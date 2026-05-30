03350768c555c4d1f17e43effb7b92b33be03363
facux-an <fandrada153@gmail.com>
2026-05-30 15:19:13 -0300

    Fix: Remove loaddata from build.sh to prevent build crash

--- build.sh
+++ build.sh
@@ -11,8 +11,6 @@ python manage.py collectstatic --no-input
 echo "Aplicando migraciones a PostgreSQL..."
 python manage.py migrate
 
-echo "Cargando backup definitivo..."
-python manage.py loaddata backup_final.json
 
 # Bloque para crear el superusuario automáticamente sin consola
 if [[ -n "${DJANGO_SUPERUSER_USERNAME}" ]] && [[ -n "${DJANGO_SUPERUSER_PASSWORD}" ]] && [[ -n "${DJANGO_SUPERUSER_EMAIL}" ]]; then
