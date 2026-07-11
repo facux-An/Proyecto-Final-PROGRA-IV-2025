b4fd70ecb0ae356b92f19b4a71b35b4d652bfe3c
facux-an <fandrada153@gmail.com>
2026-07-11 11:40:03 -0300

    actualizacion de mensajes lindos en zoho mails despliegue

--- biblioteca_plus/urls.py
+++ biblioteca_plus/urls.py
@@ -40,7 +40,10 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     
     # Recuperación de contraseña (Zoho SMTP)
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='auth/password_reset_form.html'), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='auth/password_reset_form.html',
+        html_email_template_name='auth/password_reset_email.html'
+    ), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'), name='password_reset_complete'),
